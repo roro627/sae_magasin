50f7a40293fb7d4df92f1a9f7d9c419d3277d300
roro627 <Romain.lambert27@laposte.net>
2024-05-07 14:25:21 +0200

    ajout de travail du 07/05 à 14h30

--- application_1/SoftwareModel.py
+++ application_1/SoftwareModel.py
@@ -1,43 +1,41 @@
-import os,json
+import os,json,sys
 
 # -----------------------------------------------------------------------------
 # --- class Outil
 # -----------------------------------------------------------------------------
 
-class Outil():
+class projet():
     
     # constructeur
     def __init__(self) -> None:
         
         # attributs
         
-        # liste des produits
         self.nom_projet: str = ""
         self.auteur: str = ""
-        self.date: str = ""
+        # TODO: METTRE TYPE DATE
+        self.date = ""
         self.nom: str = ""
         self.magasin: str = ""
         self.liste: list[str] = []
         
+        self.filePath:str = ""
         
-        
-    def getListe(self) -> list :
-        return self.liste
     
     # sauvegarder les informations du projet en cours dans un fichier ecrit en json.
-    def saveFile(self,filePath) -> None :
-        with open(filePath, 'w') as f:
-            content = {"nom_projet": "Projet 1",
-                        "auteur": "Moi",
-                        "date": "2021-10-01",
-                        "nom": "Lambert",
-                        "magasin": "Brico",
+    def saveFile(self) -> None :
+        with open(self.filePath, 'w', encoding='utf-8') as f:
+            content = {"nom_projet": self.nom_projet,
+                        "auteur": self.auteur,
+                        "date": self.date,
+                        "nom": self.nom,
+                        "magasin": self.magasin,
                         "liste": self.liste}
             json.dump(content, f)
             
     # charger les informations du projet en cours depuis un fichier json.
-    def loadFile(self,filePath) -> None :
-        with open(filePath, 'r') as f:
+    def loadFile(self) -> None :
+        with open(self.filePath, 'r', encoding='utf-8') as f:
             content = json.load(f)
             self.nom_projet = content["nom_projet"]
             self.auteur = content["auteur"]
@@ -46,6 +44,54 @@ class Outil():
             self.magasin = content["magasin"]
             self.liste = content["liste"]
             
+    # supprimer le fichier du projet en cours en vérifiant qu'il existe.
+    def deleteFile(self) -> None :
+        if self.filePath != "" and os.path.exists(self.filePath):
+            os.remove(self.filePath)
+            
+            self.nom_projet = ""
+            self.auteur = ""
+            self.date = ""
+            self.nom = ""
+            self.magasin = ""
+            self.liste = []
+            
+            self.filePath = ""
+    
+            
     # ajouter un produit a la liste des produits du projet en cours.
     def addProduct(self,product) -> None :
         self.liste.append(product)
+        
+    # supprimer un produit de la liste des produits du projet en cours.
+    def removeProduct(self,product) -> None :
+        self.liste.remove(product)
+        
+    
+    def update(self,objet):
+        self.nom_projet = objet.nom_projet
+        self.auteur = objet.auteur
+        self.date = objet.date
+        self.nom = objet.nom
+        self.magasin = objet.magasin
+        self.liste = objet.liste
+        self.filePath = objet.filePath
+    
+if __name__ == "__main__":
+    # test de la classe Projet
+    print('TEST: class Projet')
+    mon_projet_test = projet()
+    mon_projet_test.filePath = sys.path[0] + '/projet_test.json'
+    mon_projet_test.nom_projet = 'Projet de test'
+    mon_projet_test.auteur = 'Moi'
+    mon_projet_test.date = '2021-10-01'
+    mon_projet_test.nom = 'MonNom'
+    mon_projet_test.magasin = 'MonMagasin'
+    
+    mon_projet = projet()
+    
+    # avant update
+    print('mon_projet :', mon_projet.__dict__)    
+    mon_projet.update(mon_projet_test)
+    # après update
+    print('mon_projet :', mon_projet.__dict__)
