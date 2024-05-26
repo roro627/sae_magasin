3e7294be30e26e651c8922abf153ad2c19b548b2
SIAME <siame.romain.scw@gmail.com>
2024-05-26 14:39:12 +0200

    La fonction qui renvoie un dico (graphes) pour avoir les coordonnées des produits

--- V2/PlateauToGraph.py
+++ V2/PlateauToGraph.py
@@ -1,55 +1,23 @@
-import networkx as nx
-import matplotlib.pyplot as plt
+def create_graph_dict(plateau):
+    graph_dict = {}
 
-
-def create_graph(plateau):
-    graph = nx.Graph()
-
-    # Ajoute des sommets au graphe en fonction des objets dans le plateau
-    for row in plateau:
-        for column in row:
-            if isinstance(column, list):
-                for objet in column:
-                    graph.add_node(objet)
-                    
-
-    # Ajoute des arêtes au graphe en fonction des objets dans le plateau
+    # Ajoute les noeuds au dictionnaire du graphe en fonction des coordonnées
     for i in range(len(plateau)):
         for j in range(len(plateau[i])):
+            graph_dict[(i, j)] = []
             if isinstance(plateau[i][j], list):
-                for objet in plateau[i][j]:
-                    for other_objet in plateau[i][j]:
-                        if objet != other_objet:
-                            graph.add_edge(objet, other_objet, weight=1)  
+                graph_dict[(i, j)] = plateau[i][j]
+            elif plateau[i][j] == 0:
+                graph_dict[(i, j)].append(0)
 
-    return graph
+    return graph_dict
 
-def display_graph(graph):
-    pos = nx.spring_layout(graph)
-    nx.draw(graph, pos, with_labels=True)
-    plt.show()   
-
-
-products = [
-    [0, 0, 0, ["Objet 1", "Objet 2"]],
-    [0, 0, 0, ["Objet 3"]],
-    [0, 0, ["Objet 3"], 0]
+# Exemple d'utilisation
+plateau = [
+    [0, 0, ["Objet 1", 'Objet 2']],
+    [0, 0, ["Objet 3"]],
+    [["Objet 3"], 0, 0]
 ]
 
-graph = create_graph(products)
-
-print("Nombre de sommets:", graph.number_of_nodes())
-print("Nombre d'arêtes:", graph.number_of_edges())
-
-# Affiche les informations sur les sommets et les arêtes du graphe
-
-
-for node in graph.nodes():
-    print("Sommet:", node)
-    
-    for neighbor in graph.neighbors(node):
-        
-        print("Arête:", node, "->", neighbor)
-        print("Pondération:", graph.get_edge_data(node, neighbor)['weight'])
-        
-display_graph(graph)
+graph_dict = create_graph_dict(plateau)
+print(graph_dict)
