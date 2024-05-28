7522390a0670f00baa5ca4718b5982aa62dc2521
SIAME <siame.romain.scw@gmail.com>
2024-05-28 11:00:15 +0200

    début de dijkstra

--- V2/PlateauToGraph.py
+++ V2/PlateauToGraph.py
@@ -1,23 +1,60 @@
-def create_graph_dict(plateau):
-    graph_dict = {}
+graph = {
+    (0, 0): {
+        'adjacence': {(0, 1): 1, (1, 0): 1},
+        'produit': ['pomme', 'banane']
+    },
+    (0, 1): {
+        'adjacence': {(0, 0): 1, (1, 1): 1},
+        'produit': ['orange']
+    },
+    (1, 0): {
+        'adjacence': {(0, 0): 1, (1, 1): 1},
+        'produit': ['champignon']
+    },
+    (1, 1): {
+        'adjacence': {(0, 1): 1, (1, 0): 1},
+        'produit': ['pomme']
+    }
+}
 
-    # Ajoute les noeuds au dictionnaire du graphe en fonction des coordonnées
-    for i in range(len(plateau)):
-        for j in range(len(plateau[i])):
-            graph_dict[(i, j)] = []
-            if isinstance(plateau[i][j], list):
-                graph_dict[(i, j)] = plateau[i][j]
-            elif plateau[i][j] == 0:
-                graph_dict[(i, j)].append(0)
+def dijkstra(graph, start, food_list):
+    distances = {node: float('inf') for node in graph}
+    distances[start] = 0
+    visited = set()
 
-    return graph_dict
+    while visited != set(graph):
+        current_node = min((node for node in graph if node not in visited), key=lambda node: distances[node])
+        visited.add(current_node)
 
-# Exemple d'utilisation
-plateau = [
-    [0, 0, ["Objet 1", 'Objet 2']],
-    [0, 0, ["Objet 3"]],
-    [["Objet 3"], 0, 0]
-]
+        for neighbor, weight in graph[current_node]['adjacence'].items():
+            distance = distances[current_node] + weight
+            if distance < distances[neighbor]:
+                distances[neighbor] = distance
 
-graph_dict = create_graph_dict(plateau)
-print(graph_dict)
+    # Find the shortest path to collect all the food
+    shortest_path = []
+    current_node = start
+    while food_list:
+        neighbors = graph[current_node]['adjacence']
+        closest_neighbors = [neighbor for neighbor in neighbors if graph[neighbor]['produit']]
+        if closest_neighbors:
+            closest_neighbor = min(closest_neighbors, key=lambda neighbor: distances[neighbor])
+            shortest_path.append(closest_neighbor)
+            current_node = closest_neighbor
+            food_list = [food for food in food_list if food not in graph[closest_neighbor]['produit']]
+            if len(food_list) == 0:
+                break
+        else:
+            shortest_path.append(current_node)
+            current_node = min(neighbors, key=lambda neighbor: distances[neighbor])
+
+    # Remove two food items from the list
+    if len(food_list) >= 2:
+        food_list = food_list[:2]
+
+    return shortest_path
+
+# Utilisation de l'algorithme Dijkstra
+food_list = ['pomme', 'banane', 'orange', 'champignon']
+shortest_path = dijkstra(graph, (0, 0), food_list)
+print(shortest_path)
